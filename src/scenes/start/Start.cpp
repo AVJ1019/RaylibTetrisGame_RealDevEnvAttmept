7a3ca7b7cc8d57ae2b01e6034bd631d03bc23d51
Aske Johansen <202405193@post.au.dk>
2026-03-01 20:43:03 +0100

    Removed reliance on internal cols and rows

--- src/scenes/start/Start.cpp
+++ src/scenes/start/Start.cpp
@@ -1,9 +1,9 @@
 #include "Start.h"
 #include <iostream>
 
-Start::Start(AssetMananger *am, int rows, int cols) : Scene(am), row(rows), col(cols)
+Start::Start(AssetMananger *am) : Scene(am)
 {
-    playArea = new Board(row, col);
+    playArea = new Board(ROWS, COLS);
     playArea->update();
     // Sådan skal nye brikker indsættes
     auto offsets = playArea->getGridOffset();
@@ -20,27 +20,27 @@ void Start::draw(Renderer *renderer)
 void Start::update(float dt)
 {
     static float tPass = 0;
-    if (IsKeyPressed(KEY_LEFT) && activePiece->canMove(playArea->getGrid(), -1, 0, 0, col))
+    if (IsKeyPressed(KEY_LEFT) && activePiece->canMove(playArea->getGrid(), -1, 0, 0))
         std::cout << "[START] Tetromino moved LEFT...\n";
-    if (IsKeyPressed(KEY_RIGHT) && activePiece->canMove(playArea->getGrid(), 1, 0, 0, col))
+    if (IsKeyPressed(KEY_RIGHT) && activePiece->canMove(playArea->getGrid(), 1, 0, 0))
         std::cout << "[START] Tetromino moved RIGHT...\n";
     if (IsKeyPressed(KEY_SPACE))
-        while (activePiece->canMove(playArea->getGrid(), 0, 1, 0, col))
+        while (activePiece->canMove(playArea->getGrid(), 0, 1, 0))
         {
         }
-    if (IsKeyPressed(KEY_UP) && activePiece->canMove(playArea->getGrid(), 0, 0, 1, col))
+    if (IsKeyPressed(KEY_UP) && activePiece->canMove(playArea->getGrid(), 0, 0, 1))
         std::cout << "[START] Tetromino ROTATED...\n";
-    else if (IsKeyPressed(KEY_UP) && !activePiece->canMove(playArea->getGrid(), 0, 0, 1, col))
+    else if (IsKeyPressed(KEY_UP) && !activePiece->canMove(playArea->getGrid(), 0, 0, 1))
     {
-        // activePiece->canMove(playArea->getGrid(), 0, -1, 0, col);
-        activePiece->canMove(playArea->getGrid(), 0, 0, 1, col);
+        // activePiece->canMove(playArea->getGrid(), 0, -1, 0); // Hvis den skal opad?
+        activePiece->canMove(playArea->getGrid(), 0, 0, 1);
         std::cout << "[START] Tetromino moved UP and ROTATED...\n";
     }
     if (tPass < (dt * dropRate))
         tPass += dt;
     else
     {
-        activePiece->canMove(playArea->getGrid(), 0, 1, 0, col);
+        activePiece->canMove(playArea->getGrid(), 0, 1, 0);
         tPass = 0;
     }
     if (!activePiece->isAlive())
